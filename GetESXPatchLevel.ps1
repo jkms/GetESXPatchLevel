77bfeab22a9233a702786d5c1b18f08346edf2aa
John <john@jkms.me>
2014-04-21 15:15:53 -0700

    Initial Creation

---
+++ GetESXPatchLevel.ps1
@@ -0,0 +1,5 @@
+$vmhosts = Get-VMHost | Sort Name | Where-Object {$_.State -eq "Connected"}
+foreach ($vmhost in $vmhosts){
+	$esxcli = Get-ESXCLI -VMHost ld001esxprd07.ld.corp.local
+	$esxcli.software.vib.list() | export-csv "$vmhost.name.csv"
+}
