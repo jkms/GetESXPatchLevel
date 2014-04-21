49e33f4f0b5128a51e3bb4bf21bf664dded44f50
John <john@jkms.me>
2014-04-21 15:35:11 -0700

    Script now works

--- GetESXPatchLevel.ps1
+++ GetESXPatchLevel.ps1
@@ -1,7 +1,22 @@
-Connect-VIServer ld001vcenter01
+# ------------------------------------------------------------------------
+# NAME: GetESXPatchLevel.ps1
+# AUTHOR: John Stafford
+# DATE:04/21/2014
+#
+# COMMENTS: This script will prompt a user for a VCenter Server
+# and then query all of the hosts for their patch level, and dump
+# it in a CSV file. Simple.
+#
+# ------------------------------------------------------------------------
+
+$vcenterserver = Read-Host 'Which vcenter server would you like to query?'
+Connect-VIServer $vcenterserver
+
+$outputfolder = "CSV"
 
 $vmhosts = Get-VMHost | Sort Name | Where-Object {$_.State -eq "Connected"}
 foreach ($vmhost in $vmhosts){
 	$esxcli = Get-ESXCLI -VMHost ld001esxprd07.ld.corp.local
-	$esxcli.software.vib.list() | export-csv "$vmhost.name.csv"
+	$esxcli.software.vib.list() | export-csv "$outputfolder\$vmhost.name.csv"
 }
+Disconnect-VIServer -confirm:$false
